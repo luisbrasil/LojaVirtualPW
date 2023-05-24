99bf5c5c0fe7ef0968913747e494b656c6fcbd5f
Luis Fernando Brasil <luis4756@live.com>
2023-05-23 22:26:26 -0300

    Finalização PermissaoPessoas

--- src/main/java/com/LojaVirtual/entities/PermissaoPessoa.java
+++ src/main/java/com/LojaVirtual/entities/PermissaoPessoa.java
@@ -1,6 +1,8 @@
 package com.LojaVirtual.entities;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -8,15 +10,20 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Entity
 @Table(name = "permissao_pessoa")
+@Data
+@EqualsAndHashCode(callSuper = false)
 public class PermissaoPessoa extends EntidadeBase {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
     @ManyToOne
+    @JsonIgnore
     @JoinColumn(name = "idPessoa")
     private Pessoa pessoa;
 
--- src/main/java/com/LojaVirtual/entities/Pessoa.java
+++ src/main/java/com/LojaVirtual/entities/Pessoa.java
@@ -2,15 +2,19 @@ package com.LojaVirtual.entities;
 
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Setter;
 
 @Entity
 @Table(name = "pessoa")
@@ -31,7 +35,17 @@ public class Pessoa extends EntidadeBase {
     @ManyToOne
     private Cidade cidade; 
 
+    @OneToMany(mappedBy = "pessoa", orphanRemoval = true, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @Setter(value = AccessLevel.NONE)
+    private List<PermissaoPessoa> permissaoPessoas;
+
     @ManyToMany
     List<Permissao> permissoes;
 
+    public void setPermissaoPessoas(List<PermissaoPessoa> permissaoPessoas){
+        for(PermissaoPessoa permissaoPessoa:permissaoPessoas){
+            permissaoPessoa.setPessoa(this);
+        }
+        this.permissaoPessoas = permissaoPessoas;
+    }
 }
